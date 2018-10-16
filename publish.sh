d71c24e0b7e8be23010d384e2fe78ac06a4c6689
dongmingchao <1657883517@qq.com>
2018-10-16 16:00:17 +0800

    测试travis使用gh-pages

--- publish.sh
+++ publish.sh
@@ -21,6 +21,6 @@ git commit -m "Deploy to GitHub Pages $(date)"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:demo
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 
 exit $?
