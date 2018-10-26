7a052ce677ea809c9ad8f5af7d00e8d73f244230
dongmingchao <1657883517@qq.com>
2018-10-27 02:25:27 +0800

    pwa dist/build.js路径问题

--- webpack.config.js
+++ webpack.config.js
@@ -96,7 +96,7 @@ module.exports = {
             filename: 'service-worker.js',
             staticFileGlobs: ['dist/**/*.{js,html,css}'],
             minify: true,
-            stripPrefix: '/dist/'
+            stripPrefix: 'dist/'
         })
     ]
 };
